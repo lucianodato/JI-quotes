b62188414d8b48f16b6d51e24f16051e81b4c55f
Luciano Dato <lucianodato@gmail.com>
2020-07-13 21:25:51 -0300

    Added spanish translations

--- JI-quotes_es_AR.ts
+++ JI-quotes_es_AR.ts
@@ -1,3 +1,151 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="JI-quotes_es_AR"></TS>
+<TS version="2.1" language="es_AR">
+<context>
+    <name>ExportDialog</name>
+    <message>
+        <location filename="exportdialog.ui" line="14"/>
+        <source>Export Quotes</source>
+        <translation>Exportar Frases</translation>
+    </message>
+    <message>
+        <location filename="exportdialog.ui" line="20"/>
+        <source>Export All</source>
+        <translation>Exportar Todo</translation>
+    </message>
+    <message>
+        <location filename="exportdialog.ui" line="29"/>
+        <source>Export quotes</source>
+        <translation>Exportar frases</translation>
+    </message>
+    <message>
+        <location filename="exportdialog.ui" line="49"/>
+        <source>From</source>
+        <translation>Desde</translation>
+    </message>
+    <message>
+        <location filename="exportdialog.ui" line="59"/>
+        <source>to</source>
+        <translation>hasta</translation>
+    </message>
+    <message>
+        <location filename="exportdialog.ui" line="71"/>
+        <source>Export</source>
+        <translation>Exportar</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>JI-quotes</source>
+        <translation>JI-Frases</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="25"/>
+        <source>Search</source>
+        <translation>Buscar</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="35"/>
+        <source>Author</source>
+        <translation>Autor</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="42"/>
+        <source>Topic</source>
+        <translation>Tema</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="49"/>
+        <source>Content</source>
+        <translation>Contenido</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="56"/>
+        <source>Enable date filtering</source>
+        <translation>Activar filtrado por fecha</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="83"/>
+        <source>Menu</source>
+        <translation>Menu</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="94"/>
+        <source>toolBar</source>
+        <translation>Barra de Herramientas</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="111"/>
+        <source>Export</source>
+        <translation>Exportar</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="116"/>
+        <source>Quit</source>
+        <translation>Salir</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="121"/>
+        <source>Topics</source>
+        <translation>Temas</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="126"/>
+        <source>Add</source>
+        <translation>Agregar</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="131"/>
+        <source>Edit</source>
+        <translation>Modificar</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="136"/>
+        <source>Remove</source>
+        <translation>Eliminar</translation>
+    </message>
+</context>
+<context>
+    <name>QuoteDialog</name>
+    <message>
+        <location filename="quotedialog.ui" line="14"/>
+        <source>Quote</source>
+        <translation>Frase</translation>
+    </message>
+    <message>
+        <location filename="quotedialog.ui" line="20"/>
+        <source>Content</source>
+        <translation>Contenido</translation>
+    </message>
+    <message>
+        <location filename="quotedialog.ui" line="30"/>
+        <source>Author</source>
+        <translation>Autor</translation>
+    </message>
+    <message>
+        <location filename="quotedialog.ui" line="40"/>
+        <source>Topic</source>
+        <translation>Tema</translation>
+    </message>
+</context>
+<context>
+    <name>TopicsDialog</name>
+    <message>
+        <location filename="topicsdialog.ui" line="14"/>
+        <source>Topics</source>
+        <translation>Temas</translation>
+    </message>
+    <message>
+        <location filename="topicsdialog.ui" line="20"/>
+        <source>Add</source>
+        <translation>Agregar</translation>
+    </message>
+    <message>
+        <location filename="topicsdialog.ui" line="27"/>
+        <source>Remove</source>
+        <translation>Eliminar</translation>
+    </message>
+</context>
+</TS>
--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    QApplication application(argc, argv);
+
+    QTranslator translator;
+    translator.load(QLocale::system(), QStringLiteral("JI-quotes_"));
+    application.installTranslator(&translator);
+
+    MainWindow windows;
+    windows.show();
+    return application.exec();
 }
