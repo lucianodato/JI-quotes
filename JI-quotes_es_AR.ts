75745d5f18f20471052aaac2f74cea43ee5faff9
Luciano Dato <lucianodato@gmail.com>
2020-08-03 08:59:20 -0300

    i18n work

--- JI-quotes_es_AR.ts
+++ JI-quotes_es_AR.ts
@@ -2,6 +2,77 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="es_AR">
 <context>
+    <name>ConfigurationDialog</name>
+    <message>
+        <location filename="configurationdialog.ui" line="14"/>
+        <source>Configurations</source>
+        <translation>Configuraciones</translation>
+    </message>
+    <message>
+        <location filename="configurationdialog.ui" line="24"/>
+        <source>Document Introduction</source>
+        <oldsource>Recopilation Introduction</oldsource>
+        <translation>Introduccion del documento</translation>
+    </message>
+    <message>
+        <location filename="configurationdialog.ui" line="34"/>
+        <source>Document Author</source>
+        <oldsource>Recopilation Author</oldsource>
+        <translation>Autor del documento</translation>
+    </message>
+    <message>
+        <location filename="configurationdialog.ui" line="59"/>
+        <source>Save</source>
+        <translation>Guardar</translation>
+    </message>
+    <message>
+        <location filename="configurationdialog.ui" line="72"/>
+        <source>Document Title</source>
+        <translation>Titulo del documento</translation>
+    </message>
+    <message>
+        <location filename="configurationdialog.ui" line="82"/>
+        <source>Set basic configurations for the document that is going to be generated</source>
+        <translation>Configure los campos del documento a generar</translation>
+    </message>
+    <message>
+        <location filename="configurationdialog.cpp" line="11"/>
+        <source>JI-Quotes</source>
+        <translation>Juan Ignacio Frases</translation>
+    </message>
+</context>
+<context>
+    <name>DbManager</name>
+    <message>
+        <location filename="dbmanager.cpp" line="16"/>
+        <location filename="dbmanager.cpp" line="31"/>
+        <location filename="dbmanager.cpp" line="50"/>
+        <location filename="dbmanager.cpp" line="58"/>
+        <source>Database Error</source>
+        <translation>Error de base de datos</translation>
+    </message>
+    <message>
+        <location filename="dbmanager.cpp" line="17"/>
+        <source>Database driver not found</source>
+        <translation>Controlador de base de datos no encontrado</translation>
+    </message>
+    <message>
+        <location filename="dbmanager.cpp" line="32"/>
+        <source>There was an error creating or opening the database</source>
+        <translation>Se encontro un error creando o abriendo la base de datos</translation>
+    </message>
+    <message>
+        <location filename="dbmanager.cpp" line="51"/>
+        <source>Error creating tables</source>
+        <translation>Error creando tablas</translation>
+    </message>
+    <message>
+        <location filename="dbmanager.cpp" line="59"/>
+        <source>Error creating indexes</source>
+        <translation>Error creando indices</translation>
+    </message>
+</context>
+<context>
     <name>ExportDialog</name>
     <message>
         <location filename="exportdialog.ui" line="14"/>
@@ -9,29 +80,44 @@
         <translation>Exportar Frases</translation>
     </message>
     <message>
-        <location filename="exportdialog.ui" line="20"/>
+        <location filename="exportdialog.ui" line="24"/>
         <source>Export All</source>
         <translation>Exportar Todo</translation>
     </message>
     <message>
-        <location filename="exportdialog.ui" line="29"/>
+        <location filename="exportdialog.ui" line="36"/>
         <source>Export quotes</source>
         <translation>Exportar frases</translation>
     </message>
     <message>
-        <location filename="exportdialog.ui" line="49"/>
+        <location filename="exportdialog.ui" line="56"/>
         <source>From</source>
         <translation>Desde</translation>
     </message>
     <message>
-        <location filename="exportdialog.ui" line="63"/>
+        <location filename="exportdialog.ui" line="73"/>
         <source>to</source>
         <translation>hasta</translation>
     </message>
     <message>
-        <location filename="exportdialog.ui" line="82"/>
+        <location filename="exportdialog.ui" line="110"/>
         <source>Export</source>
         <translation>Exportar</translation>
+    </message>
+    <message>
+        <location filename="exportdialog.cpp" line="27"/>
+        <source>Save PDF location</source>
+        <translation>Lugar para guardar el PDF</translation>
+    </message>
+    <message>
+        <location filename="exportdialog.cpp" line="27"/>
+        <source>Pdf Files (*.pdf)</source>
+        <translation>Archivo PDF (*.pdf)</translation>
+    </message>
+    <message>
+        <location filename="exportdialog.cpp" line="28"/>
+        <source>M-d-yy</source>
+        <translation>d-M-yy</translation>
     </message>
 </context>
 <context>
@@ -46,24 +132,82 @@
         <translation type="vanished">Buscar</translation>
     </message>
     <message>
-        <source>Author</source>
-        <translation type="vanished">Autor</translation>
+        <location filename="mainwindow.cpp" line="47"/>
+        <source>quoteId</source>
+        <translation>quoteId</translation>
     </message>
     <message>
+        <location filename="mainwindow.cpp" line="49"/>
+        <source>Quote</source>
+        <translation>Frase</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="51"/>
+        <source>Author</source>
+        <translation>Autor</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="53"/>
+        <source>Creation Date</source>
+        <translation>Fecha de creacion</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="55"/>
         <source>Topic</source>
-        <translation type="vanished">Tema</translation>
+        <translation>Tema</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="69"/>
+        <source>topicId</source>
+        <translation>topicId</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="71"/>
+        <source>Name</source>
+        <translation>Nombre</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="75"/>
+        <source>Data Error</source>
+        <translation>Error de Datos</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="76"/>
+        <source>Error loading database data into applications</source>
+        <translation>Error leyendo la informacion de la base de datos</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="124"/>
+        <source>Edit quote</source>
+        <translation>Modificar frase</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="125"/>
+        <location filename="mainwindow.cpp" line="135"/>
+        <source>Please select a quote first</source>
+        <translation>Seleccione una frase primero</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="134"/>
+        <location filename="mainwindow.cpp" line="140"/>
+        <source>Remove quote</source>
+        <translation>Eliminar frase</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="141"/>
+        <source>Are you sure you want to delete this quote?</source>
+        <translation></translation>
     </message>
     <message>
         <source>Content</source>
         <translation type="vanished">Contenido</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="59"/>
         <source>Enable date filtering</source>
-        <translation>Activar filtrado por fecha</translation>
+        <translation type="vanished">Activar filtrado por fecha</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="94"/>
+        <location filename="mainwindow.ui" line="90"/>
         <source>Menu</source>
         <translation>Menu</translation>
     </message>
@@ -77,45 +221,49 @@
         <translation>Filtre la frase por campo</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="66"/>
         <source>MM/dd/yyyy</source>
         <oldsource>dd/MM/yyyy</oldsource>
-        <translation>dd/MM/yyyy</translation>
+        <translation type="vanished">dd/MM/yyyy</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="105"/>
+        <location filename="mainwindow.ui" line="102"/>
         <source>ToolBar</source>
         <translation>Barra de herramientas</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="129"/>
+        <location filename="mainwindow.ui" line="127"/>
         <source>Export</source>
         <translation>Exportar</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="138"/>
+        <location filename="mainwindow.ui" line="136"/>
         <source>Quit</source>
         <translation>Salir</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="147"/>
+        <location filename="mainwindow.ui" line="145"/>
         <source>Topics</source>
         <translation>Temas</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="156"/>
+        <location filename="mainwindow.ui" line="154"/>
         <source>Add</source>
         <translation>Agregar</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="165"/>
+        <location filename="mainwindow.ui" line="163"/>
         <source>Edit</source>
         <translation>Modificar</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="174"/>
+        <location filename="mainwindow.ui" line="172"/>
         <source>Remove</source>
         <translation>Eliminar</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="181"/>
+        <source>Configuration</source>
+        <translation>Configuraciones</translation>
     </message>
 </context>
 <context>
@@ -126,38 +274,41 @@
         <translation>Frase</translation>
     </message>
     <message>
-        <location filename="quotedialog.ui" line="20"/>
+        <location filename="quotedialog.ui" line="24"/>
         <source>Content</source>
         <translation>Contenido</translation>
     </message>
     <message>
-        <location filename="quotedialog.ui" line="27"/>
         <source>Write the quote here</source>
-        <translation>Escriba la frase aqui</translation>
+        <translation type="vanished">Escriba la frase aqui</translation>
     </message>
     <message>
-        <location filename="quotedialog.ui" line="34"/>
+        <location filename="quotedialog.ui" line="31"/>
+        <source>Write your quote here</source>
+        <translation>Escriba su frase aqui</translation>
+    </message>
+    <message>
+        <location filename="quotedialog.ui" line="38"/>
         <source>Author</source>
         <translation>Autor</translation>
     </message>
     <message>
-        <location filename="quotedialog.ui" line="41"/>
+        <location filename="quotedialog.ui" line="45"/>
         <source>Write the author for the quote</source>
         <oldsource>Write the author for the quote </oldsource>
         <translation>Escriba el autor de la frase</translation>
     </message>
     <message>
-        <location filename="quotedialog.ui" line="48"/>
+        <location filename="quotedialog.ui" line="52"/>
         <source>Topic</source>
         <translation>Tema</translation>
     </message>
     <message>
-        <location filename="quotedialog.ui" line="64"/>
         <source>Cancel</source>
-        <translation>Cancelar</translation>
+        <translation type="vanished">Cancelar</translation>
     </message>
     <message>
-        <location filename="quotedialog.ui" line="75"/>
+        <location filename="quotedialog.ui" line="81"/>
         <source>Save</source>
         <translation>Guardar</translation>
     </message>
@@ -170,19 +321,18 @@
         <translation>Temas</translation>
     </message>
     <message>
-        <location filename="topicsdialog.ui" line="25"/>
+        <location filename="topicsdialog.ui" line="29"/>
         <source>Add</source>
         <translation>Agregar</translation>
     </message>
     <message>
-        <location filename="topicsdialog.ui" line="36"/>
+        <location filename="topicsdialog.ui" line="40"/>
         <source>Remove</source>
         <translation>Eliminar</translation>
     </message>
     <message>
-        <location filename="topicsdialog.ui" line="60"/>
         <source>Cancel</source>
-        <translation>Cancelar</translation>
+        <translation type="vanished">Cancelar</translation>
     </message>
 </context>
 </TS>
